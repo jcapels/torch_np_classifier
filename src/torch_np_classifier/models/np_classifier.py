4d857f283aa761ec28f2e50851d0ce8a0604073c
jcapels <jcapels96@gmail.com>
2026-05-22 14:47:54 +0100

    [FIX] np classifier module

--- src/torch_np_classifier/models/np_classifier.py
+++ src/torch_np_classifier/models/np_classifier.py
@@ -1,9 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import BCELoss
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-from plants_sm.models.lightning_model import InternalLightningModule
 
 
 class NPClassifierDNN(nn.Module):
@@ -56,48 +53,3 @@ class NPClassifierDNN(nn.Module):
         if return_embedding:
             return x, embedding
         return x
-
-
-class NPClassifier(InternalLightningModule):
-
-    def __init__(self, classification_neurons=730, metric=None, scheduler=False, original=False) -> None:
-        self.classification_neurons = classification_neurons
-        self.scheduler = scheduler
-        self.return_embedding = False
-        self._contructor_parameters = {}
-        self.original = original
-        super().__init__(metric=metric)
-        self._create_model()
-
-    def _update_constructor_parameters(self):
-        self._contructor_parameters.update({
-            'classification_neurons': self.classification_neurons,
-            'metric': self.metric,
-            'scheduler': self.scheduler,
-            'original': self.original,
-        })
-
-    def _create_model(self):
-        self.np_classifier_model = NPClassifierDNN(self.classification_neurons, original=self.original)
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam([{'params': self.np_classifier_model.parameters()}], lr=0.00001)
-        if self.scheduler:
-            scheduler = {'scheduler': ReduceLROnPlateau(optimizer, 'min'), 'monitor': 'val_loss'}
-            return [optimizer], [scheduler]
-        return optimizer
-
-    def forward(self, x):
-        return self.np_classifier_model(x)
-
-    def compute_loss(self, logits, y):
-        return BCELoss()(logits, y)
-
-    def predict_step(self, batch):
-        if len(batch) == 2:
-            inputs, target = batch
-        else:
-            inputs = batch
-        if not isinstance(inputs, list):
-            inputs = [inputs]
-        return self.np_classifier_model(inputs, return_embedding=self.return_embedding)
